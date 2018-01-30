2d8a58363733dac4251f04daa36759297c083b21
Chuck Charles <bmd2018@hotmail.com>
2018-01-30 14:28:46 -0500

    Something

--- src/com/week2/Main.java
+++ src/com/week2/Main.java
@@ -1,7 +1,16 @@
 package com.week2;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        int[] highTemp = {45, 29, 10, 22, 41, 28, 33};
+        int[] precipitation = {95, 60, 25, 5, 0, 75, 90};
+        String[] dayOfTheWeek = {"Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday", "Sunday"};
+        for (int i = 0; i < 6; i++) {
+            if (highTemp[i] < 32 && precipitation[i] > 50) {
+                System.out.println("It might snow " + dayOfTheWeek[i]);
+            }
+        }
+
 
     }
 
